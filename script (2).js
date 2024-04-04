94ea2676d624d46d79bb65222d38f082664cb754
NITH2810 <156092743+NITH2810@users.noreply.github.com>
2024-04-04 10:31:38 +0530

    Add files via upload

---
+++ script (2).js
@@ -0,0 +1,52 @@
+// search-box open close js code
+let navbar = document.querySelector(".navbar");
+let searchBox = document.querySelector(".search-box .bx-search");
+// let searchBoxCancel = document.querySelector(".search-box .bx-x");
+
+searchBox.addEventListener("click", ()=>{
+  navbar.classList.toggle("showInput");
+  if(navbar.classList.contains("showInput")){
+    searchBox.classList.replace("bx-search" ,"bx-x");
+  }else {
+    searchBox.classList.replace("bx-x" ,"bx-search");
+  }
+});
+
+// sidebar open close js code
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+menuOpenBtn.onclick = function() {
+navLinks.style.left = "0";
+}
+menuCloseBtn.onclick = function() {
+navLinks.style.left = "-100%";
+}
+
+
+// sidebar submenu open close js code
+let htmlcssArrow = document.querySelector(".htmlcss-arrow");
+htmlcssArrow.onclick = function() {
+ navLinks.classList.toggle("show1");
+}
+let moreArrow = document.querySelector(".more-arrow");
+moreArrow.onclick = function() {
+ navLinks.classList.toggle("show2");
+}
+let jsArrow = document.querySelector(".js-arrow");
+jsArrow.onclick = function() {
+ navLinks.classList.toggle("show3");
+}
+var indexValue = 0;
+function slideShow(){
+  setTimeout(slideShow, 2500);
+  var x;
+  const img = document.querySelectorAll(".im");
+  for(x = 0; x < img.length; x++){
+    img[x].style.display = "none";
+  }
+  indexValue++;
+  if(indexValue > img.length){indexValue = 1}
+  img[indexValue -1].style.display = "block";
+}
+slideShow();
